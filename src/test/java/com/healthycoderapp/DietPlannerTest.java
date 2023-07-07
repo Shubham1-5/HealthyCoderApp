09dc749e53b02c8d872871d1ee8c55431427bce6
Shubham Singh <singh.shbhm90@gmail.com>
2023-07-08 00:03:54 +0530

    @RepeatedTest

--- src/test/java/com/healthycoderapp/DietPlannerTest.java
+++ src/test/java/com/healthycoderapp/DietPlannerTest.java
@@ -2,6 +2,7 @@ package com.healthycoderapp;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,6 +23,26 @@ class DietPlannerTest {
 	
 	@Test
 	void should_ReturnCorrectDietPlan_When_CorrectCoder() {
+		
+		// given
+		Coder coder = new Coder(1.82, 75.0, 26, Gender.MALE);
+		DietPlan expected = new DietPlan(2202, 110, 73, 275);
+		
+		// when
+		DietPlan actual = dietPlanner.calculateDiet(coder);
+		
+		// then
+		assertAll(
+				() -> assertEquals(expected.getCalories(), actual.getCalories()),
+				() -> assertEquals(expected.getProtein(), actual.getProtein()),
+				() -> assertEquals(expected.getFat(), actual.getFat()),
+				() -> assertEquals(expected.getCarbohydrate(), actual.getCarbohydrate())
+		);
+		
+	}
+	
+	@RepeatedTest(value = 10, name = RepeatedTest.LONG_DISPLAY_NAME)
+	void should_ReturnCorrectDietPlan_When_CorrectCoder2() {
 		
 		// given
 		Coder coder = new Coder(1.82, 75.0, 26, Gender.MALE);
