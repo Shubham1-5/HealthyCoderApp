344bca84439014d5d141f8509ddf4de62192573c
Shubham Singh <singh.shbhm90@gmail.com>
2023-07-07 20:04:42 +0530

    Test following necessary conventions

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -7,8 +7,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class BMICalculatorTest {
 	
 	@Test
-	void test() {
-		assertTrue(BMICalculator.isDietRecommended(81.2, 1.65));
+	void should_ReturnTrue_When_DietRecommended() {
+		
+		// given
+		double weight = 81.2;
+		double height = 1.65;
+		
+		// when
+		boolean recommended = BMICalculator.isDietRecommended(weight, height);
+		
+		// then
+		assertTrue(recommended);
+	}
+	
+	@Test
+	void should_ReturnFalse_When_DietNotRecommended() {
+		
+		// given
+		double weight = 50.0;
+		double height = 1.92;
+		
+		// when
+		boolean recommended = BMICalculator.isDietRecommended(weight, height);
+		
+		// then
+		assertFalse(recommended);
 	}
 	
 }
